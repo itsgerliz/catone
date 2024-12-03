939b3f671c7364e057e1271eb99dbac4d800c953
itsgerliz <gerworks.hs@gmail.com>
2024-12-03 13:37:57 +0100

    adv(main.rs): Advancements in argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct CliArgs {
+    #[arg(id = "arch", help = "Archive?", short = 'a', long = "archive", required_unless_present = "unarch")]
+    do_archive: bool,
+    #[arg(id = "unarch", help = "Unarchive?", short = 'u', long = "unarchive", required_unless_present = "arch")]
+    do_unarchive: bool,
+    #[arg(id = "verb", help = "Increase verbosity", short = 'v', long = "verbose", required = false)]
+    verbose: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    CliArgs::parse();
 }
