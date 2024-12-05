3f85e2040cecc0a1715ae0e6ff717c92ee5f752d
itsgerliz <gerworks.hs@gmail.com>
2024-12-05 12:51:08 +0100

    adv(main.rs): Added arguments for the archive path and validating archives

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::process::exit;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -7,10 +8,24 @@ struct CliArgs {
     do_archive: bool,
     #[arg(id = "unarch", help = "Unarchive?", short = 'u', long = "unarchive", required_unless_present = "arch")]
     do_unarchive: bool,
+    #[arg(id = "val", help = "Validate the archive's structure, this overrides other arguments", short = 'c', long = "check", required = false)]
+    validate: bool,
     #[arg(id = "verb", help = "Increase verbosity", short = 'v', long = "verbose", required = false)]
-    verbose: bool
+    verbose: bool,
+    #[arg(id = "file", help = "File path (absolute or relative) to archive", required = true)]
+    file: String
 }
 
 fn main() {
-    CliArgs::parse();
+    let args = CliArgs::parse();
+    if args.validate == true {
+        // Future
+    } else if args.do_archive && args.do_unarchive == true {
+        eprintln!("Cannot archive and unarchive at the same time!");
+        exit(1);
+    } else if args.do_archive == true {
+        // Future
+    } else if args.do_unarchive == true {
+        // Future
+    }
 }
